f39dd5874ab2aa6b9247f7c9b4e26e86d4a33f9d
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-05-22 17:59:05 +0700

    [FEATURE] working on modal

--- src/Companies/Company Detail/CompanyDetail.jsx
+++ src/Companies/Company Detail/CompanyDetail.jsx
@@ -1,11 +1,29 @@
+import { IconEdit, IconPencilBolt } from "@tabler/icons-react";
 import "../Company Detail/CompanyDetailStyle.css";
+import { useState } from "react";
+import { ModalComp } from "./ModalComp";
 
 export const CompanyDetail = () => {
+    const [openModal,setOpenModal] = useState(true);
     return(
         <div className="cx mt-5">
         <div className="container xcntr">
             <div className="insdx gap-5 text-white">
-                <h1>Company Name</h1>
+                <div className="d-flex justify-content-between">
+                    <h1>Company Name</h1>
+                    <button style={{
+                        border:1,
+                        width:45,
+                        backgroundColor: "transparent",
+                        color: "white"
+                    }}
+                    >
+                        <IconEdit/>
+                    </button>
+
+                    <ModalComp open={openModal}/>
+
+                </div>
                 <h6>Company address</h6>
                 <h6>Company phone number</h6>
                 <h6>Company status</h6>
---
+++ src/Companies/Company Detail/ModalComp.jsx
@@ -0,0 +1,70 @@
+import "../Company Detail/ModalStyle.css";
+export const ModalComp = ({open}) => {
+    if(!open) return null
+   
+    return(
+        <>
+            <div className="overlay">
+                <div className="modalContainer text-black p-4">
+                    <h2>Please Insert the new desired information</h2>
+                        <div className="d-flex modal-input">
+                            <div className="d-flex flex-column mt-4">
+                                <label>enter the new name</label>
+                                <input className="mdl-input mt-1" 
+                                style={{
+                                    borderStyle:"solid",
+                                    borderWidth:1,
+                                    borderColor: "#bebebe"
+                                }}
+                                placeholder="name"></input>
+                            </div>
+
+                            <div className="d-flex flex-column mt-1">
+                                <label>enter the new address</label>
+                                <input className="mdl-input"
+                                style={{
+                                    borderStyle:"solid",
+                                    borderWidth:1,
+                                    borderColor: "#bebebe"
+                                }}
+                                placeholder="address"></input>
+                            </div>
+
+                            <div className="d-flex flex-column mt-1">
+                                <label>enter the new phone number</label>
+                                <input className="mdl-input" 
+                                style={{
+                                    borderStyle:"solid",
+                                    borderWidth:1,
+                                    borderColor: "#bebebe"
+                                }}
+                                placeholder="phone number"></input>
+                            </div>
+
+                            <div className="d-flex flex-column mt-1">
+                                <label>enter the new status</label>
+                                <input className="mdl-input"
+                                style={{
+                                    borderStyle:"solid",
+                                    borderWidth:1,
+                                    borderColor: "#bebebe"
+                                }}
+                                placeholder="status"></input>
+                            </div>
+
+                            <div className="mt-3 d-flex gap-2 justify-content-end">
+                                <button className="confirmed-button text-white bg-success"
+                                onMouseEnter={() => {handleGlow(bxn1)}} 
+                                onMouseLeave={() => {handleOff(bxn1)}}
+                                >submit</button>
+                                <button className="confirmed-button text-white bg-danger"
+                                onMouseEnter={() => {handleGlow(bxn2)}} 
+                                onMouseLeave={() => {handleOff(bxn2)}}
+                                >cancel</button>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </>
+    )
+}
