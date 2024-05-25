7aa3f0bb6f15b40b32a8a475e9c3de684fbaf83f
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-05-25 16:31:21 +0700

    [REVISED] Revised model of button

--- src/Batch/Bar/Bar.jsx
+++ src/Batch/Bar/Bar.jsx
@@ -1,12 +1,54 @@
 import { IconPlus, IconTrashFilled } from "@tabler/icons-react";
 import "../Bar/Bar.css";
+import { useRef,useEffect } from "react";
+import gsap from "gsap";
 
 export const Bar = () => {
+
+    const addBtn = useRef(null);
+    const rmvBtn = useRef(null);
+
+    useEffect(()=>{
+        gsap.set(addBtn.current, {
+            x: 0,
+            color:"#00bfff",
+            backgroundColor:"white",
+            borderWidth:1,
+                borderStyle:"solid",
+                borderColor:"#00bfff"
+        })
+        gsap.set(rmvBtn.current, {
+            x: 0,
+            color:"#00bfff",
+            backgroundColor:"white",
+            borderWidth:1,
+                borderStyle:"solid",
+                borderColor:"#00bfff"
+        })
+    },[])
+
+    const inHvr = (element) => {
+        gsap.to(element,{
+            x:10,
+            color:"white",
+            backgroundColor:"#00bfff",
+            boxShadow: "0px 6px 8px rgba(0, 0, 0, 0.15)"
+        })
+    }
+
+    const outHvr = (element) => {
+        gsap.to(element,{
+            x:0,
+            color:"#00bfff",
+            backgroundColor:"white",
+            boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)"
+        })
+    }
     return (
         <>
         <div className="d-flex flex-column ms-5 justify-content-center gap-2">
-            <div><button className="btn-batch"><IconPlus/></button></div>
-            <div><button className="btn-batch"><IconTrashFilled/></button></div>
+            <div onMouseEnter={() => {inHvr(addBtn.current)}} onMouseLeave={() => {outHvr(addBtn.current)}}><button className="btn-batch ms-5" ref={addBtn}><IconPlus/></button></div>
+            <div onMouseEnter={() => {inHvr(rmvBtn.current)}} onMouseLeave={() => {outHvr(rmvBtn.current)}}><button className="btn-batch ms-5" ref={rmvBtn}><IconTrashFilled/></button></div>
         </div>
         <div className="batch-container">
             <ul className="d-flex gap-2" >
