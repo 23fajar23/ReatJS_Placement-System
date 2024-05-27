84977cf3a410d574b931763b3c76b276178bc331
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-05-27 12:02:43 +0700

    [REVISED, FIX] fixed navigation bar and re-modeled navigation bar

--- src/nvbr/NavBar.jsx
+++ src/nvbr/NavBar.jsx
@@ -1,35 +1,54 @@
 import { IconBrandTeams, IconBuildingSkyscraper, IconHome, IconIdBadge2, IconMessage, IconUser, IconUsersGroup } from "@tabler/icons-react"
 import "../nvbr/Nvb.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import gsap from "gsap";
 
 export const NavBar = () => {
     const iconsRefs = useRef([])
+    const [indicator,setIndicator] = useState(null);
 
     const onHover = (index) => {
         gsap.to(iconsRefs.current[index],{
-            backgroundColor:"red"
+            backgroundColor:"#4cd2ff",
+            duration: 0.5,
+            borderRadius:5,
+            width:100
         })
+        setIndicator(index);
     }
     const offHover = (index) => {
         gsap.to(iconsRefs.current[index],{
-            backgroundColor:"white"
+            backgroundColor:"#00bfff",
+            duration: 0.3,
+            width:"auto"
         })
+        setIndicator(null);
     }
 
+    const labels = [
+        "bd team",
+        "message",
+        "clients",
+        "batch",
+        "trainees",
+        "home",
+        "user settings"
+    ]
+   
+
     const icons = [
-        <IconBrandTeams className="icn"/>,
-        <IconMessage className="icn"/>,
-        <IconBuildingSkyscraper className="icn"/>,
-        <IconUsersGroup className="icn"/>,
-        <IconIdBadge2 className="icn"/>,
-        <IconHome className="icn"/>,
-        <IconUser className="icn"/>
+        <><IconBrandTeams className="icn"/> <span>{indicator === 0 && labels[0]}</span></>,
+        <><IconMessage className="icn"/> <span>{indicator === 1 && labels[1]}</span></>,
+        <><IconBuildingSkyscraper className="icn"/> <span>{indicator === 2 && labels[2]}</span></>,
+        <><IconUsersGroup className="icn"/> <span>{indicator === 3 && labels[3]}</span></>,
+        <><IconIdBadge2 className="icn"/> <span>{indicator === 4 && labels[4]}</span></>,
+        <><IconHome className="icn"/> <span>{indicator === 5 && labels[5]}</span></>,
+        <><IconUser className="icn"/> <span>{indicator === 6 && labels[6]}</span></>
     ]
 
     return (
         <>
-        <div className="d-flex justify-content-between px-4 rounded-pill nvbr" >
+        <div className="d-flex gap-5 justify-content-between px-4 rounded-pill nvbr" >
             {icons.map((icon,index)=>(
                 <div key={index} 
                 onMouseEnter={() =>{onHover(index)}} 
