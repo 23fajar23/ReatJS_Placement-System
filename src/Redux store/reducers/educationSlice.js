7e10e12fdc5d1b392dd613655d7d421508703bb0
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-03 08:19:03 +0700

    [IMPROVE] Changed API path to localhost path

--- src/Redux store/reducers/educationSlice.js
+++ src/Redux store/reducers/educationSlice.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 //fetch education data slice
 export const fetchEducationData = createAsyncThunk('education/fetchEducationData', async () => {
   const token = localStorage.getItem('token');
-  const response = await axios.get('http://10.10.102.254:8080/api/education/all', {
+  const response = await axios.get('http://localhost:8080/api/education/all', {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
@@ -18,7 +18,7 @@ export const fetchEducationData = createAsyncThunk('education/fetchEducationData
 export const deleteEducationData = createAsyncThunk('education/deleteEducationData', async (name) => {
     const token = localStorage.getItem('token');
 
-    const response = await axios.get('http://10.10.102.254:8080/api/education/all', {
+    const response = await axios.get('http://localhost:8080/api/education/all', {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
@@ -31,7 +31,7 @@ export const deleteEducationData = createAsyncThunk('education/deleteEducationDa
         throw new Error('Education not found');
     }
 
-    await axios.delete(`http://10.10.102.254:8080/api/education/${educationToDelete.id}`,{
+    await axios.delete(`http://localhost:8080/api/education/${educationToDelete.id}`,{
         headers:{
             'Content-Type' :'application/json',
             'Authorization':`Bearer ${token}`
--- src/Service/axiosInstance.js
+++ src/Service/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:'http://10.10.102.254:8080/api'
+    baseURL:'http://localhost:8080/api'
     // baseURL:'http://localhost:5173//api'
 })
 
